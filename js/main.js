9760d83692421bbf9a0c2baa7cf43150a8f06509
TkachevNikita <ntkachev03@mail.ru>
2024-08-04 03:38:30 +0500

    feat: dynamic rendering skills section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import '../css/style.css'
 import {fetchData} from "./utils.js";
 import {renderId, renderImage, renderLanguages} from "./profile.js";
+import {renderExperiences, renderTools} from "./skills.js";
 
 const root = document.querySelector('#root');
 
@@ -13,10 +14,20 @@ const renderProfileSection = (image, id, languages) => {
     root.appendChild(profileSection);
 }
 
+const renderSkillsSection = (experience, tools) => {
+    const skillsSection = document.createElement('section');
+    skillsSection.classList.add('skills', 'row');
+    skillsSection.appendChild(experience);
+    skillsSection.appendChild(tools);
+    root.appendChild(skillsSection);
+
+}
+
 await fetchData()
     .then((response) => response.json())
     .then((data) => {
         renderProfileSection(renderImage(), renderId(data.name, data.post), renderLanguages(data.languages));
+        renderSkillsSection(renderExperiences(data.experience), renderTools(data.tools));
     });
 
 
---
+++ js/skills.js
@@ -0,0 +1,66 @@
+import {createCvBlock} from "./utils.js";
+
+export const renderExperiences = (experiences) => {
+    const content = `
+        <h2 class="cv-block__title">
+            Experience
+          </h2>
+        <ul class="experience__list">
+            ${experiences.map((experience) => {
+                return `
+                    <li class="experience__item">
+                        <div class="experience__item--container">
+                            <div class="experience__item--head">
+                                <span class="experience__item--date">
+                                    ${experience.period}
+                                    </span>
+                            </div>
+                            <div class="experience__item--body">
+                                <div class="job">
+                                    <h3 class="job__title">
+                                        ${experience.post}
+                                    </h3>
+                                    <span class="job__place">
+                                        ${experience.place}
+                                    </span>
+                                </div>
+                                <div class="competitions">
+                                    <ul class="competitions__list">
+                                        ${experience.competitions.map((competition) => {
+                                            return `<li class="competitions__item">${competition}</li>`
+                                        }).join('')}
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </li>`    
+                }).join('')}`
+
+    return createCvBlock('experience', content);
+};
+
+export const renderTools = (tools) => {
+    const content = `
+        <div class="tools">
+            <ul class="tools__categories">
+                ${tools.map((tool) => {
+                    return `
+                        <li class="tools__category">
+                            <h3 class="tools__category--title">
+                              design
+                            </h3>
+                            <ul class="tools__category--list">
+                                ${tool.tags.map((tag) => {
+                                    return `<li class="tools__category--item">
+                                        <div class="tool"></div>
+                                    </li>`;
+                                }).join('')}
+                              
+                            </ul>
+                        </li>`
+                }).join('')}
+            </ul>
+        </div>`;
+
+    return createCvBlock('tools', content);
+}
