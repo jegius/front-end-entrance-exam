4afd77790582e1a9a45fbd0c0989bc2571950b82
TkachevNikita <ntkachev03@mail.ru>
2024-08-04 13:50:44 +0500

    refactor: delete repeated code

--- js/main.js
+++ js/main.js
@@ -1,44 +1,32 @@
-import '../css/style.css'
+'use strict';
+
+import '../css/style.css';
 import {fetchData} from "./utils.js";
-import {renderId, renderImage, renderLanguages} from "./profile.js";
-import {renderExperiences, renderTools} from "./skills.js";
-import {renderEducation, renderRideSide} from "./other.js";
+import { renderId, renderImage, renderLanguages } from "./profile.js";
+import { renderExperiences, renderTools } from "./skills.js";
+import { renderEducation, renderRideSide } from "./other.js";
 
 const root = document.querySelector('#root');
 
-const renderProfileSection = (image, id, languages) => {
-    const profileSection = document.createElement('section');
-    profileSection.classList.add('profile', 'row');
-    profileSection.appendChild(image);
-    profileSection.appendChild(id);
-    profileSection.appendChild(languages)
-    root.appendChild(profileSection);
-}
+const renderSection = (className, elements) => {
+    const section = document.createElement('section');
+    section.classList.add(className, 'row');
+    elements.forEach(element => section.appendChild(element));
+    root.appendChild(section);
+};
 
-const renderSkillsSection = (experience, tools) => {
-    const skillsSection = document.createElement('section');
-    skillsSection.classList.add('skills', 'row');
-    skillsSection.appendChild(experience);
-    skillsSection.appendChild(tools);
-    root.appendChild(skillsSection);
+const loadPage = async () => {
+    try {
+        const response = await fetchData();
+        const data = await response.json();
+        renderSection('profile', [renderImage(), renderId(data.name, data.post), renderLanguages(data.languages)]);
+        renderSection('skills', [renderExperiences(data.experience), renderTools(data.tools)]);
+        renderSection('other', [renderEducation(data.education), renderRideSide(data.interests, data.email)]);
+    } catch (error) {
+        console.error(error);
+    }
+};
 
-}
-
-const renderOtherSection = (education, rightSide) => {
-    const otherSection = document.createElement('section');
-    otherSection.classList.add('other', 'row');
-    console.log(education);
-    otherSection.appendChild(education);
-    otherSection.appendChild(rightSide);
-    root.appendChild(otherSection);
-}
-
-await fetchData()
-    .then((response) => response.json())
-    .then((data) => {
-        renderProfileSection(renderImage(), renderId(data.name, data.post), renderLanguages(data.languages));
-        renderSkillsSection(renderExperiences(data.experience), renderTools(data.tools));
-        renderOtherSection(renderEducation(data.education), renderRideSide(data.interests, data.email));
-    });
-
-
+document.addEventListener('DOMContentLoaded', () => {
+    loadPage();
+});
